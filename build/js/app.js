9f0a01809be4dfd01910f0de01e63f4a2636913c
JSanchezs024 <joaquin.sanchez.suazo@gmail.com>
2025-01-14 16:46:50 -0300

    avances v.1.7

--- build/js/app.js
+++ build/js/app.js
@@ -8,3 +8,22 @@ var swiperCard = new Swiper(".swiper", {
         invert: false,
     }
 })
+
+/*Mostrar menu*/
+const nav_menu = document.getElementById('nav_menu'),
+      nav_barras = document.getElementById('nav_barras'),
+      nav_cerrar = document.getElementById('nav_cerrar')
+
+/* Menu show */
+if(nav_barras){
+    nav_barras.addEventListener('click', () =>{
+        nav_menu.classList.add('show_menu')
+   })
+}
+
+/* Menu hidden */
+if(nav_cerrar){
+    nav_cerrar.addEventListener('click', () =>{
+        nav_menu.classList.remove('show_menu')
+   })
+}
